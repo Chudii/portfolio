275a0133ce0c7a62ef2154517de0d54a5d4f2dff
Chudi <chudiibida3@gmail.com>
2022-12-26 04:01:04 -0500

    added routes to pages

---
+++ src/components/intro/Intro.jsx
@@ -0,0 +1,12 @@
+import './intro.css'
+
+const Intro = () => {
+    return (
+        <div className='i'>
+            <div className='i-left'>left</div>
+            <div className='i-right'>right</div>
+        </div>
+    )
+}
+
+export default Intro
